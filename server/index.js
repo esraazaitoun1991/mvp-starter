0622eaaa78afbe4be5ac99ac13f5b0440d289f26
esraazaitoun1991 <eng.esraaz1991@gmail.com>
2018-11-24 14:52:42 +0200

    First test

--- server/index.js
+++ server/index.js
@@ -2,12 +2,12 @@ var express = require('express');
 var bodyParser = require('body-parser');
 // UNCOMMENT THE DATABASE YOU'D LIKE TO USE
 // var items = require('../database-mysql');
-// var items = require('../database-mongo');
+var items = require('../database-mongo');
 
 var app = express();
 
 // UNCOMMENT FOR REACT
-// app.use(express.static(__dirname + '/../react-client/dist'));
+app.use(express.static(__dirname + '/../react-client/dist'));
 
 // UNCOMMENT FOR ANGULAR
 // app.use(express.static(__dirname + '/../angular-client'));
@@ -23,7 +23,7 @@ app.get('/items', function (req, res) {
   });
 });
 
-app.listen(3000, function() {
-  console.log('listening on port 3000!');
+var port = process.env.PORT || 3000 ;
+app.listen(port, function() {
+ console.log(`listening on port ${port}`);
 });
-
